a2679c8fbaf323334d92bf628a98e3980a033193
unknown <connortamminen@gmail.com>
2022-11-28 18:47:14 -0600

    finalized project, just need to clean up I think

--- main/script.js
+++ main/script.js
@@ -1,14 +1,2 @@
-// stating global variables
-var searchHistory = [];
-// create new account to get new key
-var weatherApiRootUrl = '';
-var weatherApiKey = '';
 
-// declaring DOM element references
-// update with appropriate class names from html file
-var searchForm = document.querySelector('');
-var searchInput = document.querySelector('');
-var hoyContainer = document.querySelector('');
-var weatherContainer = document.querySelector('');
-var buscaViejoContainer = document.querySelector('');
 
